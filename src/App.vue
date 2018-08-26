b6cae1c92bb389b3e37b51a3d74d345b49132451
Pedro Juan <petertitoy@protonmail.com>
2018-08-27 00:49:09 +0800

    added footer and header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <v-toolbar class="vtoolbar">
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+
     <router-view/>
+
+
+
+    <v-footer dark height="auto" class="vfooter">
+      <v-card flat tile class="indigo lighten-1 white--text text-xs-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons":key="icon" class="mx-3 white--text" icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          &copy;2018 — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </div>
 </template>
 
@@ -16,8 +48,13 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+.vtoolbar{
+  margin-bottom: 20px;
+}
+.vfooter{
+  margin-top: 20px;
 }
 </style>
