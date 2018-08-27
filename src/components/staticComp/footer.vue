7a79b4596af00e96525b57be869e8b0ac1d15be4
Pedro Juan <petertitoy@protonmail.com>
2018-08-27 20:41:05 +0800

    created static page

---
+++ src/components/staticComp/footer.vue
@@ -0,0 +1,63 @@
+<template>
+
+      <v-footer
+          dark
+          height="auto"
+        >
+          <v-card
+            class="flex"
+            flat
+            tile
+          >
+            <v-card-title class="teal">
+              <strong class="subheading" ><b>{{cardfooter}}</b></strong>
+
+              <v-spacer></v-spacer>
+
+              <v-btn
+                v-for="icon in icons"
+                :key="icon"
+                class="mx-3"
+                dark
+                icon
+              >
+                <v-icon size="24px">{{ icon }}</v-icon>
+              </v-btn>
+            </v-card-title>
+
+            <v-card-actions class="grey darken-3 justify-center">
+              &copy;{{copyRight.year}} — <strong>{{copyRight.appName}}</strong>
+            </v-card-actions>
+          </v-card>
+        </v-footer>
+
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    cardfooter :'piste nah jud ni!!!',
+    copyRight:{
+      year: 2018,
+      appName: 'hago kaau ni!'
+    },
+    icons: [
+     'fab fa-facebook',
+     'fab fa-twitter',
+     'fab fa-google-plus',
+     'fab fa-linkedin',
+     'fab fa-instagram'
+   ]
+  })
+}
+
+</script>
+<style scoped>
+
+.vfooter{
+  margin-left: auto;
+  margin-top: 20px;
+}
+
+</style>
---
+++ src/components/staticComp/header.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+
+  <v-toolbar class="vtoolbar">
+    <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+</template>
+<style scoped>
+
+.vtoolbar{
+  margin-bottom: 20px;
+}
+
+</style>
