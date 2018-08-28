d3f717ed6b921b2ff65d01543aba027d867e9759
Pedro Juan <petertitoy@protonmail.com>
2018-08-29 06:32:47 +0800

    done make component dynamic

--- src/components/staticComp/footer.vue
+++ src/components/staticComp/footer.vue
@@ -1,54 +1,52 @@
 <template>
+<v-footer
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading" ><b>{{cardfooter}}</b></strong>
 
-      <v-footer
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3"
           dark
-          height="auto"
+          icon
         >
-          <v-card
-            class="flex"
-            flat
-            tile
-          >
-            <v-card-title class="teal">
-              <strong class="subheading" ><b>{{cardfooter}}</b></strong>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
 
-              <v-spacer></v-spacer>
-
-              <v-btn
-                v-for="icon in icons"
-                :key="icon"
-                class="mx-3"
-                dark
-                icon
-              >
-                <v-icon size="24px">{{ icon }}</v-icon>
-              </v-btn>
-            </v-card-title>
-
-            <v-card-actions class="grey darken-3 justify-center">
-              &copy;{{copyRight.year}} — <strong>{{copyRight.appName}}</strong>
-            </v-card-actions>
-          </v-card>
-        </v-footer>
-
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;{{copyRight.year}} — <strong>{{copyRight.appName}}</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
 
 export default {
   data: () => ({
-    cardfooter :'piste nah jud ni!!!',
-    copyRight:{
-      year: 2018,
-      appName: 'hago kaau ni!'
-    },
-    icons: [
-     'fab fa-facebook',
-     'fab fa-twitter',
-     'fab fa-google-plus',
-     'fab fa-linkedin',
-     'fab fa-instagram'
-   ]
+  cardfooter :'piste nah jud ni!!!',
+  copyRight:{
+    year: 2018,
+    appName: 'hago kaau ni!'
+  },
+  icons: [
+   'fab fa-facebook',
+   'fab fa-twitter',
+   'fab fa-google-plus',
+   'fab fa-linkedin',
+   'fab fa-instagram'
+ ]
   })
 }
 
