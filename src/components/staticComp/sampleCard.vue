916328eed7d6ae09504369bee96f289a400b24d9
Pedro Jun <peter.damondamon@davao.sti.edu>
2018-08-31 00:49:40 +0800

    added sample card header

---
+++ src/components/staticComp/sampleCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Top western road trips</div>
+            <span class="grey--text">1,000 miles of wonder</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="purple">Explore</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn icon @click="show = !show">
+            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-slide-y-transition>
+          <v-card-actions v-show="show">
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="purple">Explore</v-btn>
+          </v-card-actions>
+        </v-slide-y-transition>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+  export default {
+    data: () => ({
+      show: false
+    })
+  }
+</script>
