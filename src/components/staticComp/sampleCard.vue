74e78c06a9fa00530ac98593efd21d59bfdc6726
Pedro Juan <petertitoy@protonmail.com>
2018-09-01 05:05:54 +0800

    finalized card for products

--- src/components/staticComp/sampleCard.vue
+++ src/components/staticComp/sampleCard.vue
@@ -3,21 +3,23 @@
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
         <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          :src="product.image"
           height="200px"
+          :alt="product.name"
         >
         </v-img>
 
         <v-card-title primary-title>
           <div>
-            <div class="headline">Top western road trips</div>
-            <span class="grey--text">1,000 miles of wonder</span>
+            <div class="headline">{{product.name}}</div>
+            <span class="grey--text">{{product.subinfo}}</span>
           </div>
         </v-card-title>
 
         <v-card-actions>
-          <v-btn flat>Share</v-btn>
-          <v-btn flat color="purple">Explore</v-btn>
+          <v-btn color="primary"> Add Product
+            <v-icon light right>check_circle</v-icon>
+          </v-btn>
           <v-spacer></v-spacer>
           <v-btn icon @click="show = !show">
             <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
@@ -25,17 +27,11 @@
         </v-card-actions>
 
         <v-slide-y-transition>
-          <v-card v-show="show">
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            height="10%"
-          >
-          <v-card-actions>
-            <v-btn flat>Share</v-btn>
-            <v-btn flat color="purple">Explore</v-btn>
-          </v-card-actions>
-          </v-img>
-          </v-card>
+        <v-card-text v-show="show">
+            <ul v-for="elem in product.descrip">
+              <li>{{elem.item}}</li>
+            </ul>
+         </v-card-text>
         </v-slide-y-transition>
       </v-card>
     </v-flex>
@@ -44,7 +40,22 @@
 <script>
   export default {
     data: () => ({
-      show: false
+      show: false,
+      product: {
+        image:'https://firebasestorage.googleapis.com/v0/b/fukthesis.appspot.com/o/cctv1.png?alt=media&token=07547908-b19e-44d0-b02e-57af7001c920',
+        name: 'HAC-PFW3601-A180-AC24',
+        subinfo: '3X2MP Multi-Sensor Panoramic HDCVI IR-Bullet Camera',
+        descrip: [
+          {item: 'Three 1/2.8 2Megapixel progressive scan CMOS'},
+          {item: '1 panoramic view &3 individual 2MP video output simultaneously'},
+          {item: 'Max. 15fps@4K,25fps@4M'},
+          {item: '120db true WDR,2D&3D NR'},
+          {item: 'HDCVI starlight technology'},
+          {item: 'Max.20m IR distance'},
+          {item: 'IP67&IK10 ingress protection'},
+          {item: 'Up to 500m transmission with RG59 coaxial cable'}
+        ]
+      }
     })
   }
 </script>
