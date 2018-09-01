3dcd7f9e7925eefa04211c191511459e68ebcf16
Pedro Juan <petertitoy@protonmail.com>
2018-09-02 03:23:15 +0800

    finish configure mainboard router

--- src/components/routes/mainBoard.vue
+++ src/components/routes/mainBoard.vue
@@ -1,6 +1,118 @@
 <template>
-  
+  <v-app id="inspire">
+    <v-navigation-drawer
+        :clipped="$vuetify.breakpoint.lgAndUp"
+        v-model="drawer"
+        fixed
+        app
+      >
+        <v-list dense>
+          <template v-for="item in items">
+            <v-layout
+              v-if="item.heading"
+              :key="item.heading"
+              row
+              align-center
+            >
+              <v-flex xs6>
+                <v-subheader v-if="item.heading">
+                  {{ item.heading }}
+                </v-subheader>
+              </v-flex>
+              <v-flex xs6 class="text-xs-center">
+                <a href="#!" class="body-2 black--text">EDIT</a>
+              </v-flex>
+            </v-layout>
+            <v-list-group
+              v-else-if="item.children"
+              v-model="item.model"
+              :key="item.text"
+              :prepend-icon="item.model ? item.icon : item['icon-alt']"
+              append-icon=""
+            >
+              <v-list-tile slot="activator">
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ item.text }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile
+                v-for="(child, i) in item.children"
+                :key="i"
+                @click=""
+              >
+                <v-list-tile-action v-if="child.icon">
+                  <v-icon>{{ child.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ child.text }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-group>
+            <v-list-tile v-else :key="item.text" :to="item.link">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+    </v-navigation-drawer>
+  <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="blue darken-3"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"> </v-toolbar-side-icon>
+        <router-link to="/mainboard/dashboard" tag="span" style="cursor: pointer" class="hidden-sm-and-down">
+          Welcome Superuser!
+        </router-link>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+    <!-- main content -->
+      <router-view></router-view>
+
+
+
+  </v-app>
 </template>
+
 <script>
 
+  export default {
+    data: () => ({
+      drawer: null,
+      dialog: false,
+      items: [
+        { icon: 'dashboard', text: 'Dashboard', link: '/mainboard/dashboard' },
+        { icon: 'contacts', text: 'Agents', link: '/mainboard/agents' },
+        { icon: 'people', text: 'Clients', link: '/mainboard/clients' },
+        { icon: 'product', text: 'Products', link: '/mainboard/products' }
+      ]
+    }),
+    props: {
+      source: String
+    }
+
+  }
 </script>
