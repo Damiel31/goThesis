f27ba7222da97202af267511a2b7c82bf9797ad5
Pedro Juan <petertitoy@protonmail.com>
2018-09-02 03:22:00 +0800

    sample native route

---
+++ src/components/routes/nativeboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title>
+        Welcome Super User!
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn>
+          <v-icon left>supervisor_account</v-icon>
+          Create Project</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
